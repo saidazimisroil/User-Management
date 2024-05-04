b618f25ce94d451d707f4bbcec73ef41ae23b266
Saidazim <134857217+saidazimisroil@users.noreply.github.com>
2024-05-04 18:06:51 +0500

    Added registration Form

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,11 +4,14 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[ORM\HasLifecycleCallbacks]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -40,6 +43,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?\DateTimeImmutable $registrationTime = null;
 
+    public function __construct()
+    {
+        $this->registrationTime = new \DateTimeImmutable();
+        $this->lastLoginTime = new \DateTimeImmutable();
+        // Initialize with default admin role
+        $this->roles = ['ROLE_ADMIN'];
+    }
+    
     public function getId(): ?int
     {
         return $this->id;
